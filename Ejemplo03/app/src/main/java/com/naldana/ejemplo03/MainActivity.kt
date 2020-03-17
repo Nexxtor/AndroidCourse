94ae552687503acaf8a4e9906a4edbaa8c562f6b
Nestor <nestor.aldana1@gmail.com>
2020-03-17 11:17:38 -0600

    Uso de click lisnteneR

---
+++ Ejemplo03/app/src/main/java/com/naldana/ejemplo03/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.naldana.ejemplo03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var nameEditText: EditText
+    lateinit var nameButton: Button
+    lateinit var nameDisplayTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bindView()
+        addEventsListener()
+
+    }
+
+    fun bindView() {
+        nameEditText =  findViewById(R.id.et_name)
+        nameButton = findViewById(R.id.bt_send_name)
+        nameDisplayTextView = findViewById(R.id.tv_display_name)
+    }
+
+    fun addEventsListener(){
+        nameButton.setOnClickListener {
+            var name = nameEditText.text
+
+            if (name.isEmpty()) {
+                nameDisplayTextView.text = getString(R.string.notification_nombre)
+            } else {
+                nameDisplayTextView.text = name
+            }
+        }
+    }
+}
